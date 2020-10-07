7bad4757591ce745791c1b29341322d17a7337c1
brandodharmasaputra <70872478+brandodharmasaputra@users.noreply.github.com>
2020-10-07 12:25:00 +0700

    Add files via upload

---
+++ Week_5/aksesDOM/js/main.js
@@ -0,0 +1,23 @@
+function myUniversal() {
+    document.querySelector("*").style.backgroundColor = "lime";
+  }
+
+  function myType() {
+    document.querySelector("h1").style.backgroundColor = "orange";
+  }
+  function myType() {
+    document.querySelector("h1").style.backgroundColor = "orange";
+  }
+  function myClass() {
+    document.querySelector(".container").style.backgroundColor = "yellow";
+  }
+  function myId() {
+    document.querySelector("#logo").style.backgroundColor = "orange";
+  }
+  function myAttribute() {
+    document.querySelector("a[target]").style.backgroundColor = "yellow";
+  }
+  function myGroup() {
+    document.querySelector("h2, h1").style.backgroundColor = "green";
+  }
+  
